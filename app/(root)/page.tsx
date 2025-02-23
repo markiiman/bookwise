d351be93f5003c736c71a2fd5f9c4488b9179853
Mark Jerly Bundalian <mbundalian@fullscale.io>
2025-02-23 21:40:05 +0800

    Add home layout

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import BookList from "@/components/BookList";
+import BookOverview from "@/components/BookOverview";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <BookOverview />
+      <BookList />
+    </>
+  );
+}
