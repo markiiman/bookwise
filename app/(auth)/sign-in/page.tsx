284548ed3489a7b0444910f94ba85520ac5ddbcc
Mark Jerly Bundalian <mbundalian@fullscale.io>
2025-02-24 17:05:58 +0800

    Add authentication forms, image upload component, and update global styles

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import AuthForm from "@/components/AuthForm";
+import { signInSchema } from "@/lib/validations";
+import React from "react";
+
+const SignIn = () => (
+  <AuthForm
+    type="SIGN_IN"
+    schema={signInSchema}
+    defaultValues={{
+      email: "",
+      password: "",
+    }}
+    onSubmit={() => {}}
+  />
+);
+
+export default SignIn;
